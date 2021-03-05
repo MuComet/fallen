8f4cfc06ec66a9830e2ec56b7ba8bb14ec1f3856
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2021-03-04 19:46:26 -0700

    Card Game actually doing things?

--- engine/instances/card_minigame.js
+++ engine/instances/card_minigame.js
@@ -4,23 +4,39 @@ class CardMinigameController extends MinigameController {
         super.onEngineCreate();
         $engine.setBackgroundColour(0xa58443);
 
-        var card_texture = ["card_1", "card_2", "card_3","card_1", "card_2", "card_3","card_1", "card_2", "card_3"];  // expand to 9 in final, or wtv needed
+        var card_texture = ["card_1", "card_2", "card_3", "card_1", "card_2", "card_3","card_1", "card_2", "card_3", "card_1", "card_2", "card_3", "card_1", "card_2", "card_3", "card_1", "card_2", "card_3"];  // expand to 9 in final, or wtv needed
         EngineUtils.shuffleArray(card_texture);
         for(var i = 0; i < card_texture.length; i++){
             var index = card_texture[i];
-            new CardBoard(50+85*i, $engine.getWindowSizeY()/2, index);
+            if(i < 9){
+                new CardBoard(70+85*i, $engine.getWindowSizeY()/2 -75, index);
+            }else{
+                new CardBoard(70+85*(i-9), $engine.getWindowSizeY()/2, index);
+            }
         }
 
         this.timer = 0;
-        this.attempts = 3;
+        this.attempts = 6;
         this.goal_index = card_texture[EngineUtils.irandom(2)];      
         this.goal_card = new PIXI.Sprite($engine.getTexture(this.goal_index));
 
         this.goal_card.x = $engine.getWindowSizeX()/2;
         this.goal_card.y = $engine.getWindowSizeY()/2 + 100;
-        $engine.createManagedRenderable(this, this.goal_card); 
-    }
+        $engine.createManagedRenderable(this, this.goal_card);
 
+        //this.startTimer(5*60);
+        var text = new PIXI.Text("Select all cards matching\nthe bottom goal card!",$engine.getDefaultTextStyle());
+        this.setInstructionRenderable(text)
+
+
+
+
+
+    }                                 
+
+    notifyFramesSkipped(frames) {
+        this.getTimer().tickDown(frames)
+    }
 
     onCreate() { 
         super.onCreate();
@@ -65,13 +81,16 @@ class CardBoard extends EngineInstance {
         if(CardMinigameController.getInstance().timer > 80){
             if(IM.instanceCollisionPoint(IN.getMouseX(), IN.getMouseY(), this)){
                 this.getSprite().tint = (0xaaaa43);
-            }else{
-                this.getSprite().tint = 0;
+                if(this.clicked == false){
+                    this.getSprite().tint = (0xffffff);
+                }
             }
 
             if(IN.mouseCheckPressed(0) && IM.instanceCollisionPoint(IN.getMouseX(), IN.getMouseY(), this)){
                 CardMinigameController.getInstance().attempts--;
-                    this.clicked = true;
+                $engine.audioPlaySound("audio/se/Miss.ogg")
+                this.clicked = true;
+                this.getSprite().tint = (0xaaaa43);
             }
             if(CardMinigameController.getInstance().attempts == 0 && this.clicked){
                 if(this.group == CardMinigameController.getInstance().goal_index){
