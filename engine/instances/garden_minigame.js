311612d429c2e4d0b2babddff181d49b0c5590a3
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2021-03-19 15:52:54 -0600

    Actually did what I said I would

--- engine/instances/garden_minigame.js
+++ engine/instances/garden_minigame.js
@@ -13,6 +13,195 @@ class GardenMinigameController extends MinigameController {
         this.waitTimer = 0;
         
         this.startTimer(60*60);
+        this.x = 90*2-100;
+        this.y = $engine.getWindowSizeY()/2 -150;
+        this.hitbox = new Hitbox(this,new RectangeHitbox(this,-25,-37,25,37));
+
+
+        var text = new PIXI.Text("Basically\n WORMS\n\nPress ENTER to cheat",$engine.getDefaultTextStyle());
+        this.setInstructionRenderable(text);
+        this.controlsUseKeyBoard(false);
+
+        this.progressText = new PIXI.Text("",$engine.getDefaultSubTextStyle());
+        $engine.createManagedRenderable(this,this.progressText);
+        this.progressText.anchor.set(0.5,0.5);
+        this.progressText.x = $engine.getWindowSizeX()/2;
+        this.progressText.y = $engine.getWindowSizeY()-30;
+        //this.updateProgressText();
+        //this.newRound();
+        var plant_array = [];
+        this.plant_array = plant_array;
+
+        for(var i = 0; i < 9; i++){
+            if(i < 3){
+                plant_array[i] = new GardenPlant(90*2+200*i, $engine.getWindowSizeY()/2 -150, i);       
+            }if(i >= 3 && i < 6){
+                plant_array[i] = new GardenPlant(90*3+200*(i-3), $engine.getWindowSizeY()/2, i);
+            }if(i >= 6 && i < 9){
+                plant_array[i] = new GardenPlant(90*2+200*(i-6), $engine.getWindowSizeY()/2 +150, i);
+            }
+        }
+
+    }
+
+    notifyFramesSkipped(frames) {
+        this.getTimer().tickDown(frames);  
+    }
+
+
+    onCreate() { 
+        super.onCreate();
+        this.onEngineCreate();
+    }
+
+
+    step() {
+        super.step();
+        if(this.minigameOver()){
+            return;
+        }
+        if(this.timer === 60){
+            var spawnI = EngineUtils.irandomRange(0,8);
+            IM.with(GardenWorm, function(worm){
+                if(worm.index === spawnI){
+                    spawnI = EngineUtils.irandomRange(0,8);;
+                }
+            });
+            this.spawnWorm(spawnI);
+            
+            //this.getTimer().unpauseTimer();
+            this.timer = 0;
+        }
+        this.moveSpray();
+        this.countPlants();
+        this.timer++;
+    }
+
+    countPlants(){
+        var temp_score = 0;
+        for(var i = 0; i < 9; i++){
+            if(this.plant_array[i] != undefined){
+                temp_score++;
+            }
+        }
+        this.score = temp_score;
+        if(this.score <= 0){
+            this.getTimer().pauseTimer();
+            this.endMinigame(false);
+        }
+    }
+
+    spawnWorm(spawnI){
+        if(spawnI < 3){
+            new GardenWorm(90*2+200*spawnI, $engine.getWindowSizeY()/2 -150, spawnI);       
+        }if(spawnI >= 3 && spawnI < 6){
+            new GardenWorm(90*3+200*(spawnI-3), $engine.getWindowSizeY()/2, spawnI);
+        }if(spawnI >= 6 && spawnI < 9){
+            new GardenWorm(90*2+200*(spawnI-6), $engine.getWindowSizeY()/2 +150, spawnI);
+        }
+    }
+
+    moveSpray(){
+        if(IN.keyCheckPressed("ArrowRight") && this.x <= 90*2+200){
+            this.x += 200;
+        }
+        else if(IN.keyCheckPressed("ArrowLeft") && this.x > 90*2){
+            this.x -= 200;
+        }
+        else if(IN.keyCheckPressed("ArrowDown") && this.y < $engine.getWindowSizeY()/2 +150){
+            if(this.x === 90*2-100 || this.x === 90*2+100 || this.x === 90*2+300){
+                this.x += 90;
+            }else{
+                this.x -= 90;
+            }
+            this.y += 150;
+        }
+        else if(IN.keyCheckPressed("ArrowUp") && this.y > $engine.getWindowSizeY()/2 -150){
+            if(this.x === 90*2-100 || this.x === 90*2+100 || this.x === 90*2+300){
+                this.x += 90;
+            }else{
+                this.x -= 90;
+            }
+            this.y -= 150;
+        }
+        
+    }
+
+
+    draw(gui, camera) {     
+        EngineDebugUtils.drawHitbox(camera,this);
+    }
+
+}
+
+class GardenWorm extends EngineInstance {
+    onCreate(x,y,index) {
+        this.x = x-100;
+        this.y = y;
+        this.index = index;
+        this.setSprite(new PIXI.Sprite($engine.getTexture("garden_worm_alive")));
+        this.hitbox = new Hitbox(this,new RectangeHitbox(this,-25,-37,25,37));
+        this.clicked = false;
+        this.wormTimer = 0;
+        this.wormTimerEat = 60;
+        this.deathTime = 0;
+    }
+
+    step(){
+        //if(!this.enabled){
+            //return;
+        //}
+        if(this.wormTimer >= this.wormTimerEat && this.deathTime === 0){            
+            GardenMinigameController.getInstance().plant_array[this.index] = undefined;
+            this.destroy();
+        }
+       
+        if(IM.instanceCollisionPoint(GardenMinigameController.getInstance().x, GardenMinigameController.getInstance().y, this) && IN.keyCheckPressed("Space")){
+            this.deathTime = this.wormTimer;
+            this.getSprite().texture = $engine.getTexture("garden_worm_dead");
+            $engine.audioPlaySound("sky_bonk");
+        }
+        if(this.wormTimer > this.deathTime + 60 && this.deathTime != 0){
+            this.destroy();
+        }
+        this.wormTimer++;
+    }
+}
+
+class GardenPlant extends EngineInstance {
+    onCreate(x,y,index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.score = 1;
+        this.setSprite(new PIXI.Sprite($engine.getTexture("garden_plant")));
+        this.hitbox = new Hitbox(this,new RectangeHitbox(this,-25,-37,25,37));
+        this.clicked = false;
+    }
+    step(){
+        if(GardenMinigameController.getInstance().plant_array[this.index] === undefined){
+            this.destroy();
+        }
+    }
+}
+
+
+
+/*class GardenMinigameController extends MinigameController {
+    
+    onEngineCreate() { 
+        super.onEngineCreate();
+        this.score = 9;
+
+        
+        new ParallaxingBackground();
+
+        this.timer = 0;
+        this.attempts = 6;
+        this.waiting = false;
+        this.waitTimer = 0;
+        
+        this.startTimer(60*60);
 
 
         var text = new PIXI.Text("Basically\n WORMS\n\nPress ENTER to cheat",$engine.getDefaultTextStyle());
@@ -148,4 +337,4 @@ class GardenPlant extends EngineInstance {
             this.destroy();
         }
     }
-}
+}*/
